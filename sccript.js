833e140e4dbb00f28e0d5a2909f3c74d44191946
Shaikhtofik2004 <shaikhtofik2454@gmail.com>
2025-05-23 22:34:33 +0530

    Add files via upload

---
+++ sccript.js
@@ -0,0 +1,29 @@
+// const element = document.querySelector('.clock');
+
+// const time = new Date();
+
+// const option = {
+//     timeZone: 'Asia/Kolkata',
+//     hour: '2-digit',
+//     minute: '2-digit',
+//     second: '2-digit'
+// }
+
+// element.textContent = time.toLocaleString('en-US', option)
+
+
+
+setInterval(() => {
+    const element = document.querySelector('.clock');
+
+    const time = new Date();
+
+    const option = {
+        timeZone: 'Asia/Kolkata',
+        hour: '2-digit',
+        minute: '2-digit',
+        second: '2-digit'
+    }
+
+    element.textContent = time.toLocaleString('en-US', option)
+}, 1000);
